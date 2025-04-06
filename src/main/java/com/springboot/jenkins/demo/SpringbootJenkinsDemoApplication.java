49091e292b2348b5b1edceab712e979b0025505e
JiangLai <3054123710@qq.com>
2025-04-06 14:40:06 +0800

    Initial commit

---
+++ src/main/java/com/springboot/jenkins/demo/SpringbootJenkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.jenkins.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJenkinsDemoApplication.class, args);
+    }
+
+}
